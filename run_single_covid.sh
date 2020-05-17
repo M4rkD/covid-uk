3274ceade97797292923dcc61af236c521509dfc
Ed Bennett <e.j.bennett@swansea.ac.uk>
2020-05-17 22:22:55 +0100

    fix errors in TMPDIR usage and termination handler in run_single_covid.sh

--- run_single_covid.sh
+++ run_single_covid.sh
@@ -6,12 +6,12 @@ ANALYSIS=$2
 NUMBER_OF_SEEDS=$3
 
 # Create a build directory in the RAM disk so run is independent
-TMPDIR=$(mktemp --directory --tmpdir=/dev/shm)
+export TMPDIR=$(mktemp --directory --tmpdir=/dev/shm)
 
 # Don't leave a mess if terminated halfway through
 term_handler() {
-  echo Time limit is up; tidying up run $1 $2...
-  rm -rf ${BUILD_DIR}
+  echo "Time limit is up; tidying up run ${SEED_INDEX} ${ANALYSIS}..."
+  rm -rf ${TMPDIR}
   exit -1
 }
 trap 'term_handler' TERM USR1
